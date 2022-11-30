c6c8b03ccf032ba451e7bbef09297b3add38a9e4
Nacho <martineztignacio@hotmail.com>
2022-11-30 20:51:07 -0300

    cambios

--- app.js
+++
@@ -1,7 +0,0 @@
-//cambiar fondo de la pagina cada 1 segundo
-var body = document.querySelector('body');
-var colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange'];
-setInterval(function () {
-    var color = colors[Math.floor(Math.random() * colors.length)];
-    body.style.backgroundColor = color;
-}, 1000);
